edfec228cfd42e8eec8fcfdd44c559ee1b688de2
Carlos Bueno <carlosbueno.contato@gmail.com>
2023-05-21 14:33:08 -0300

    adding the /auth route page

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
+<div class="">
+	<h1 class="text-center p-4">Please, authenticate youself</h1>
+	<p class="text-center p-2">
+		{#if $page.data.session}
+			{#if $page.data.session.user?.image}
+				<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
+			{/if}
+			<span class="">
+				<small class="text-primary">Signed in as</small><br />
+				<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+			</span>
+			<button class="btn btn-error" on:click={() => signOut()}>Sign out</button>
+		{:else}
+			<span class="text-primary">You are not signed in</span>
+			<br />
+			<button class="btn btn-primary" on:click={() => signIn('github')}>Sign In with GitHub</button>
+		{/if}
+	</p>
+</div>
